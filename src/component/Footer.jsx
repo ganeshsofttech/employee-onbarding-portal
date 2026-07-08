b0889856aa8ab764d23a9f002895b362836b9e92
ganeshsofttech <moreganesh327@gmail.com>
2026-07-08 05:29:41 +0000

    pass

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,10 @@
 export default function Footer() {
+  const today = new Date().toLocaleDateString();
   return (
     <footer>
       <hr />
 
-      <p>© Owned and Developed by ganeshsoftech  last update:08-07-2026</p>
+      <p>© Owned and Developed by ganeshsoftech  last update:08-07-2026 {today}</p>
     </footer>
   );
 }
