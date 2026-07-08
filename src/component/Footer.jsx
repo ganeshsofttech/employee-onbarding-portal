6711499c44a704035ab4268d8efc56e922e05db5
ganeshsofttech <42492476+ganeshsofttech@users.noreply.github.com>
2026-07-08 06:23:56 +0530

    Update footer text with new ownership details

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer>
       <hr />
 
-      <p>© 2026 Employee Onbarding Portal</p>
+      <p>© Owned and Developed by ganeshsoftech  last update:08-07-2026</p>
     </footer>
   );
 }
